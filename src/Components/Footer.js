74b3b7a6ef857285043f884c02c3308153d7f7ca
Danielle Cutler <cutlerd02@gmail.com>
2021-01-25 14:58:20 -0700

    fixed errors

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -21,17 +21,12 @@ class Footer extends Component {
             <ul className="social-links">{networks}</ul>
 
             <ul>
-              <li>Made by Danielle ♥️</li>
+              <li>Made by Danielle</li>
             </ul>
 
             <ul className="copyright">
               <li>&copy; Copyright 2020</li>
-              <li>
-                Design by{" "}
-                <a title="Styleshout" href="http://www.styleshout.com/">
-                  Styleshout
-                </a>
-              </li>
+              {}
             </ul>
           </div>
           <div id="go-top">
